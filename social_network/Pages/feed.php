2369f7dad3b887b159d799a58718d5f2f1e953f1
ddunmerr <kostyailin28@gmail.com>
2025-03-25 17:10:56 +0300

    25.03

--- social_network/Pages/feed.php
+++ social_network/Pages/feed.php
@@ -25,35 +25,60 @@ if (!empty($_GET['q'])) {
 </head>
 
 <body>
-
     <div class="container">
-        <div id="banner" class=" header"></div>
+        <!-- Сайдбар -->
         <div class="sidebar">
-            <ul>
-                <li>
-                    <a class="icon icon--home"></a>
-                </li>
-                <li>
-                    <a class="icon icon--profile"></a>
-                </li>
-                <li>
-                    <a class="icon icon--add"></a>
-                </li>
-            </ul>
+            <div class="icons-list">
+                <ul>
+                    <li><a class="icon icon--home"></a></li>
+                    <li><a class="icon icon--profile"></a></li>
+                    <li><a class="icon icon--add"></a></li>
+                </ul>
+            </div>
         </div>
+        <!-- Шапка -->
+        <div class="header"></div>
 
+        <!-- Лента -->
         <main class="feed">
-            <!-- Контент ленты -->
+            <!-- Пост 1 -->
             <div class="post">
                 <div class="post-header">
                     <div class="user">
-                        <div class="user-avatar"></div>
-                        <h1 class="user-name">Ваня денисов</h1>
+                        <div class="user-avatar" id="ivan2004"></div>
+                        <span class="user-name">Ваня Денисов</span>
                     </div>
                     <div class="edit-button"></div>
                 </div>
-                <div>
-                    <img class="post-image" src="/images/v412_61.png">
+                <img class="post-image" src="/images/v412_61.png" alt="Пост пользователя">
+                <span class="span-reaction">
+                    <button class="like-button">
+                    </button>
+                    204
+                </span>
+                <div class="post-content">
+                    <p class="post-text">Так красиво сегодня на улице! Настоящая зима)) Вспоминается Бродский: «Поздно ночью, в уснувшей долине, на самом дне, в городке, занесенном снегом по ручку двери...»</p>
+                    <span class="post-time">2 часа назад</span>
+                </div>
+            </div>
+
+            <!-- Пост 2 -->
+            <div class="post">
+                <div class="post-header">
+                    <div class="user">
+                        <div class="user-avatar" id="lizadem"></div>
+                        <span class="user-name">Лиза Дёмина</span>
+                    </div>
+                    <div class="edit-button"></div>
+                </div>
+                <img class="post-image" src="/images/v412_77.png" alt="Пост пользователя">
+                <span class="span-reaction">
+                    <button class="like-button">
+                    </button>
+                    204
+                </span>
+                <div class="post-content">
+                    <span class="post-time">1 день назад</span>
                 </div>
             </div>
         </main>
