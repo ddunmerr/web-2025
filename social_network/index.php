e76f5f4faa3741848475a76b4b62f023af21b399
ddunmerr <kostyailin28@gmail.com>
2025-05-29 00:20:10 +0300

    login_bem

--- social_network/index.php
+++ social_network/index.php
@@ -11,24 +11,25 @@
 <body>
     <div class="page">
         <!-- Изображение на заднем плане -->
-        <div class="background-image"></div>
+        <div class="page__background-image"></div>
 
         <!-- Заголовок "Войти" -->
-        <h1 class="page-title">Войти</h1>
+        <h1 class="page__title">Войти</h1>
 
         <!-- Форма входа -->
-        <form class="login-form">
-            <div class="field">
-                <label for="email">Электропочта</label>
-                <input type="email" id="email" required>
-                <p class="input-description">Введите электропочту в формате *****@******</p>
+        <form class="auth-form">
+            <div class="auth-form__field">
+                <label class="auth-form__label" for="email">Электропочта</label>
+                <input class="auth-form__input" type="email" id="email" required>
+                <p class="auth-form__description">Введите электропочту в формате *****@******</p>
             </div>
-            <div class="field">
-                <label for="password">Пароль</label>
-                <input type="password" id="password" required>
+            <div class="auth-form__field">
+                <label class="auth-form__label" for="password">Пароль</label>
+                <input class="auth-form__input" type="password" id="password" required>
             </div>
             <!--a href="pages/feed.php" class="continue-button"> Войти </a-->
-            <button onclick="window.location.href='pages/feed.php'" type="submit" class="continue-button">Продолжить</button>
+            <button onclick="window.location.href='pages/feed.php'" type="submit"
+                class="auth-form__continue-button">Продолжить</button>
         </form>
     </div>
 </body>
